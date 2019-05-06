1e05271df1b1a356e6671ec4b21c1977b0a04dce
nativ <nativ2@gmail.com>
2019-05-06 22:11:39 +0300

    change features to ORB

--- findHomography.py
+++ findHomography.py
@@ -3,31 +3,53 @@ import cv2 as cv
 import copy
 import math
 from matplotlib import pyplot as plt
+import time
 MIN_MATCH_COUNT = 10
 # img1 = cv.imread('images/cat_part.png',0)          # queryImage
 # img2 = cv.imread('images/cat.png',0) # trainImage
 # print(img1.depth())
-def findHomography(img1,img2):
+
+def getFeatures2d(feature_type):
+    print(feature_type)
+    if feature_type == "sift":
+        return cv.xfeatures2d.SIFT_create()
+    elif feature_type == "orb":
+        return cv.ORB_create()
+
+def findHomography(img1,img2, feature_type = "sift"):
     # Initiate SIFT detector
-    sift = cv.xfeatures2d.SIFT_create()
+    features = getFeatures2d(feature_type)
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    s = time.time()
+    kp1, des1 = features.detectAndCompute(img1,None)
+    kp2, des2 = features.detectAndCompute(img2,None)
+    e = time.time()
+    print("ff time is "+str(e-s))
     FLANN_INDEX_KDTREE = 1
+    s = time.time()
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks = 50)
-    flann = cv.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+    # flann = cv.FlannBasedMatcher(index_params, search_params)
+    flann = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    # print()
+    # matches = flann.knnMatch(des1,des2,k=2)
+    matches = flann.match(des1,des2)
+    e = time.time()
+    print("fmatch time is "+str(e-s))
     # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m,n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
+    # good = []
+    good = matches
+    # for m,n in matches:
+    #     if m.distance < 0.7*n.distance:
+    #         good.append(m)
 
     if len(good)>MIN_MATCH_COUNT:
+        s = time.time()
         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
         M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+        e = time.time()
+        print("fh time is "+str(e-s))
         return M, mask
     else:
         print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
@@ -47,9 +69,9 @@ def rect_size(pts):
     # print("w is: "+str(w))
     return h*w
 
-def plot_in_picture(maps,patch_im):
+def plot_in_picture(maps,patch_im,feature_type = "sift"):
     large_im = maps[-1][0];
-    M, mask = findHomography(patch_im,large_im);
+    M, mask = findHomography(patch_im,large_im, feature_type);
     if M is None:
         return None,None,None
     h,w,d = patch_im.shape
@@ -81,7 +103,7 @@ def plot_in_picture(maps,patch_im):
     return img4,M,mask
     # cv.waitKey(0)
 
-def load_video(file):
+def load_video(file, feature_type = "sift"):
     cap = cv.VideoCapture(file)
 
     maps = []
@@ -99,14 +121,17 @@ def load_video(file):
     failes = 0
     i = 0
     frame2 = 0
+    max_t = 0
     while failes < 4:
+        s = time.time()
         i+=1
         pframe = frame2
         ret, frame = cap.read()
         if ret == False:
             break;
         frame = cv.resize(frame,(int(w/3),int(h/3)))
-        frame2, M, mask = plot_in_picture(maps,frame)
+        
+        frame2, M, mask = plot_in_picture(maps,frame, feature_type)
         if frame2 is None:
             print("failllllll")
             failes+=1
@@ -115,7 +140,11 @@ def load_video(file):
         failes = 0
         print(i)
         out.write(frame2)
-
+        e = time.time()
+        if(e-s > max_t):
+            max_t = e-s
+        print("pip time is "+str(e-s))
+    print("max t is: " + str(max_t))
     cap.release()
     out.release()
 
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import findHomography as fh
 import cv2 as cv
 import numpy as np
+import time
 
 def main():
 
-    img1 = cv.imread('images/cat_part.png',0)          # queryImage
-    img2 = cv.imread('images/cat.png',0) # trainImage
+    # img1 = cv.imread('images/cat_part.png',0)          # queryImage
+    # img2 = cv.imread('images/cat.png',0) # trainImage
     # fh.plot_in_picture(img2,img1)
-    fh.load_video("images/vid2.mp4")
+    start = time.time()
+    fh.load_video("images/vid2.mp4","orb")
+    end = time.time()
+    print(end-start)
     cv.waitKey(0)
 
 
